0a1bf44be1c74d88e54a087107001fb1950b1905
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-01-11 16:26:17 +0530

    Added /me route for user.

--- backend/router/user.js
+++ backend/router/user.js
@@ -2,16 +2,25 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { User } from "../DB/schema.js"
 import dotenv from "dotenv";
+import authenticateJwt from "../middleware/auth.js";
 dotenv.config();
 
 
 const router = express.Router();
 const SECRET = process.env.SECRET;
 
-
-router.get("/", (req, res) => {
-    return res.json({ msg: "This is home page" });
-
+router.get("/me", authenticateJwt, async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.user.username });
+        if (!user) {
+            return res.status(403).json({ msg: "User doesn't exist" });
+        }
+        console.log(user);
+        res.json({ username: user.username });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Server error" });
+    }
 });
 
 router.post("/login", async (req, res) => {
